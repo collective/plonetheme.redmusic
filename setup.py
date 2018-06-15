037eb2a2fc4bfc341e270a7e86d545855a0e2f96
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2018-06-15 17:13:38 -0400

    Added more strings classifiers items for this packages

--- setup.py
+++ setup.py
@@ -5,14 +5,17 @@ version = '1.1'
 
 setup(
         name='plonetheme.redmusic',
-        description='An installable Diazo theme for Plone 4.1',
+        description='Redmusic Theme, is an installable Diazo theme for Plone 4',
         long_description=open('README.rst', 'rb').read()+'\n'+
                          open(os.path.join("docs", "INSTALL.txt")).read()+'\n'+
                          open(os.path.join("docs", "HISTORY.txt")).read(),
         version='1.1',
         author='Giacomo Spettoli',
         author_email='giacomo.spettoli@gmail.com',
-        url='http://svn.plone.org/svn/collective/plonetheme.redmusic',
+        maintainer='Leonardo Caballero',
+        maintainer_email='leonardocaballero@gmail.com',
+        url='https://github.com/giacomos/plonetheme.redmusic',
+        license='GPL',
         packages=find_packages(),
         include_package_data=True,
         namespace_packages=[
@@ -22,13 +25,29 @@ setup(
             'setuptools',
             'plone.app.theming',
             ],
+        # Get more strings from
+        # https://pypi.org/pypi?:action=list_classifiers
         classifiers=[
+            "Development Status :: 3 - Alpha",
+            "Environment :: Web Environment",
             "Framework :: Plone",
+            "Framework :: Plone :: 4.1",
+            "Framework :: Plone :: 4.2",
+            "Framework :: Plone :: 4.3",
+            "Framework :: Plone :: Theme",
+            "Framework :: Zope2",
+            "Framework :: Zope3",
+            "Intended Audience :: Developers",
+            "Intended Audience :: End Users/Desktop",
+            "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+            "Operating System :: OS Independent",
             "Programming Language :: Python",
+            "Programming Language :: Python :: 2.6",
+            "Programming Language :: Python :: 2.7",
+            "Topic :: Internet",
+            "Topic :: Software Development :: Libraries :: Python Modules",
             ],
         entry_points={
             'z3c.autoinclude.plugin': 'target = plone',
             }
         )
-
-
